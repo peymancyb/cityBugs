fbec5bd247aa4a596a303269df3fb4d6d00b1017
MahtaMR <zohreh.torabian7366@gmail.com>
2017-12-30 20:13:19 +0400

    splash modification && firebase added

---
+++ src/backend/firebase.js
@@ -0,0 +1,11 @@
+import * as firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyASOtmjvpnaF2xfIfnkwuS59oZASF49tN8",
+    authDomain: "citybug-b6b52.firebaseapp.com",
+    databaseURL: "https://citybug-b6b52.firebaseio.com",
+    projectId: "citybug-b6b52",
+    storageBucket: "",
+    messagingSenderId: "689165223615"
+  };
+  firebase.initializeApp(config);
