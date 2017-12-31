e9a083e2909724cc16ecdd5b90cdb9b3c2ecfa53
peymancyb <paymanghazvini@yahoo.com>
2018-01-01 01:31:30 +0400

    current position activated!

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -1,5 +1,5 @@
 import React , {Component} from 'react';
-import { View , Text , Dimensions} from 'react-native';
+import { View , Text , Dimensions, Alert } from 'react-native';
 import styles from './style';
 import { MapComponent } from '../components/MapContainer';
 import { MapView } from 'expo';
@@ -11,19 +11,22 @@ const SCREEN_WIDTH = width;
 const SCREEN_HEIGHT = height;
 const ASPECT_RATIO = width/height;
 
-const LATITUDE_DELTA = 0.0922;
-const LONGITUDE_DELTA = 0.0421;
-
 
 export default class Main extends Component{
   constructor(props){
     super(props);
     this.state={
+      currentLocation:{
+        latitude: 40.1792,
+        longitude: 44.4991,
+        longitudeDelta:0.0421,
+        latitudeDelta:0.0922,
+      },
       mapRegion:{
         latitude:40.1792,
         longitude:44.4991,
-        longitudeDelta:LONGITUDE_DELTA,
-        latitudeDelta:LATITUDE_DELTA,
+        longitudeDelta:0.0421,
+        latitudeDelta:0.0922,
       },
       markers: [{
           key:1,
@@ -45,6 +48,7 @@ export default class Main extends Component{
         }]
     }
     this._onRegionChange=this._onRegionChange.bind(this);
+    this._currentLocation=this._currentLocation.bind(this);
   }
 
 _onRegionChange(region){
@@ -53,32 +57,72 @@ _onRegionChange(region){
   }));
 }
 
-componentWillUnmount(){
+_currentLocation(){
+  navigator.geolocation.getCurrentPosition(
+    //success
+    (position)=>{
+    let currentPosition = {
+      latitude:position.coords.latitude,
+      longitude:position.coords.longitude,
+      longitudeDelta:0.0421,
+      latitudeDelta:0.0922,
+    };
+    this.setState({
+      currentLocation: currentPosition
+    });
+    // console.log("your location is:\n"+position.coords.latitude+"\n"+position.coords.longitude);
+  },
+  //error handler
+  (error)=>{
+    if(error.code == "E_LOCATION_SERVICES_DISABLED"){
+      Alert.alert(
+        "CityBugs",
+        "CityBugs needs to know your location, please turn on your Location service!",
+        [
+          {text:"OK",onPress: ()=>Alert.alert(
+            "CityBugs",
+            "Thank you!",
+            [
+              {text:"OK"},
+            ]
+          )},
+          {text:"Ask me later"},
+        ],
+        { cancelable: false }
+      );
+    }
+  },
+  //options
+  {enableHighAccuracy:true, timeout:5000 , maximumAge:0}
+);
+}
 
+componentDidMount(){
+  this._currentLocation();
 }
 
   render(){
+    // console.log(this.state.currentLocation);
     return(
-      <MapView
-        style={{ flex: 1 }}
-        showsUserLocation={true}
-        followUserLocation={true}
-        zoomEnabled={true}
-        provider={MapView.PROVIDER_GOOGLE}
-        region={this.state.mapRegion}
-        onRegionChange={this._onRegionChange}
-      >
-        {this.state.markers.map((marker) => (
-          <MapView.Marker
-            coordinate={marker.coordinates}
-            key={marker.key}
-            title={marker.title}
-            description={marker.description}
-          />
-        ),
-
-      )}
-      </MapView>
+        <MapView
+          style={{ flex: 1 }}
+          showsUserLocation={true}
+          followUserLocation={true}
+          zoomEnabled={true}
+          provider={MapView.PROVIDER_GOOGLE}
+          initialRegion={this.state.currentLocation}
+          onRegionChange={this._onRegionChange}
+        >
+          {this.state.markers.map((marker)=>(
+            <MapView.Marker
+              coordinate={marker.coordinates}
+              key={marker.key}
+              title={marker.title}
+              description={marker.description}
+            />
+          ),
+        )}
+        </MapView>
     );
   }
 
